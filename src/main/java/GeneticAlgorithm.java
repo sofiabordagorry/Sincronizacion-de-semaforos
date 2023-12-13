4580cc3affede906174b50cedaf21ea4bba5f1d2
pachecus <126217007+pachecus@users.noreply.github.com>
2023-12-13 20:27:23 -0300

    Update GeneticAlgorithm.java

--- src/main/java/GeneticAlgorithm.java
+++ src/main/java/GeneticAlgorithm.java
@@ -16,28 +16,45 @@ import java.util.LinkedHashMap;
 public class GeneticAlgorithm implements Algorithm<DoubleSolution>{
 	private List<DoubleSolution> initialPopulation;
 	private int maxGenerations;
+	private SinglePoint	crossover;
+	private UniformMutation mutation;
 	
-	public GeneticAlgorithm(List<DoubleSolution> initialPopulation,int maxGenerations) {
+	public GeneticAlgorithm(List<DoubleSolution> initialPopulation,int maxGenerations, double crossProbability, double mutationProbability, int perturbation) {
 		this.initialPopulation = initialPopulation;
 		this.maxGenerations = maxGenerations;
+		this.crossover = new SinglePointCrossover(crossProbabilty);
+		this.mutation = new UniformMutation(mutationProbability, perturbation);
 	}
 	
 	public void run() {
-
 		List<DoubleSolution> currentPopulation = this.initialPopulation;
 		for(int i=0; i < this.maxGenerations;i++) {
 			 // Evaluar la aptitud de la población actual
-			Map<DoubleSolution, Double> rankings = evaluarPoblacion(currentPopulation);
-
-            // Seleccionar padres
-
-            // Cruzar los padres para crear descendientes
-
+			List<DoubleSolution> poblacionRankeada = evaluarPoblacion(currentPopulation);
+			
+			// Rankear la poblacion actual para elegir padres
+			// Aplicar cruzamientos y crear nueva generacion
+			List<DoubleSolution> offspringPopulation = new  ArrayList<>();
+			for(int j = 0; i  < (poblacionRankeada.length / 2); j++) {//Se elije la mejor mitad de los padres
+				List<DoubleSolution> parents = Arrays.asList(currentPopulation.get(j), currentPopulation.get(j+1));
+				List<DoubleSolution> children = this.crossover.execute(parents);
+				offspringPopulation.addAll(children);
+			}
+			
+			List<DoubleSolution> p1 = Arrays.asList(currentPopulation.get(0), currentPopulation.get(2));
+			List<DoubleSolution> ch1 = this.crossover.execute(p1);
+			List<DoubleSolution> p2 = Arrays.asList(currefntPopulation.get(1), currentPopulation.get(3));
+			List<DoubleSolution> ch2 = this.crossover.execute(p2);
+			offspringPopulation.addAll(ch1);
+			offspringPopulation.addAll(ch2);
+			
+			for(DoubleSolution s : offspringPopulation) {
+				
+			}
+			
             // Aplicar mutación a los descendientes
 
-            // Evaluar la aptitud de los descendientes
-
-            // Reemplazar la población actual con los descendientes si son mejores
+			currentPopulation = offspringPopulation;
 		}
 	}
 
@@ -56,7 +73,7 @@ public class GeneticAlgorithm implements Algorithm<DoubleSolution>{
 		return null;
 	}
 	
-    public Map<DoubleSolution, Double> evaluarPoblacion(List<DoubleSolution> currentPopulation) {
+    public List<DoubleSolution> evaluarPoblacion(List<DoubleSolution> currentPopulation) {
         Map<DoubleSolution, Double> ranks = new HashMap<>();
 
         // Calcular la velocidad promedio de cada solución y almacenarla en el mapa
@@ -74,9 +91,9 @@ public class GeneticAlgorithm implements Algorithm<DoubleSolution>{
         Collections.sort(sortedEntries, Comparator.comparing(Map.Entry::getValue, Collections.reverseOrder()));
 
         // Crear un nuevo mapa ordenado para almacenar las soluciones y velocidades
-        Map<DoubleSolution, Double> sortedRanks = new LinkedHashMap<>();
+        List<DoubleSolution> sortedRanks = new ArrayList<>();
         for (Map.Entry<DoubleSolution, Double> entry : sortedEntries) {
-            sortedRanks.put(entry.getKey(), entry.getValue());
+            sortedRanks.put(entry.getKey());
         }
 
         // Ahora, 'sortedRanks' contiene las soluciones y velocidades ordenadas según las velocidades
